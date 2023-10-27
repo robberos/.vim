f78eb0d0019c453e7f4020f03c89483bd25423a8
Robert Rosengren <robertr@axis.com>
2023-10-27 08:45:05 +0200

    Remove bashisms from tmux_vim_copy
    Change-Id: I1b37cfa47aa4a17ea794352d707f89e8584ab29a

--- tmux_vim_copy.sh
+++ tmux_vim_copy.sh
@@ -7,18 +7,18 @@ search_string=$(echo "$3" | sed -e 's/</\\</g' -e 's/>/\\>/g')
 session=0
 # Fix for "Press any key"
 tmux send-keys Enter
-for row in `tmux list-panes -F '#{session_name};#{window_index};#{pane_active};#{pane_pid}'`;
+for args in `tmux list-panes -F '#{window_index};#{pane_active};#{pane_pid}'`;
 do
-  args=(${row//;/ })
-  echo $args
-  active=${args[2]}
-  pane="${args[1]}.$session"
-  has_vim=`ps --ppid ${args[3]} | grep -c vim`
+  set -f; IFS=';'
+  set -- $args
+  active=$2
+  pane="$1.$session"
+  has_vim=`ps --ppid $3 | grep -c vim`
 
   # choose first inactive pane running vim
   # FIXME if no pane has vim, start it?
   # FIXME if multiple panes running vim, toggle better where to open file?
-  if [ $active == '0' ] && [ $has_vim == '1' ]; then
+  if [ "$active" = "0" ] && [ "$has_vim" = "1" ]; then
     tmux send-keys -t $pane Escape
     tmux send-keys -t $pane :tabnew Space $file Enter
     tmux send-keys -t $pane /"$search_string" Enter
@@ -28,5 +28,5 @@ do
   fi
 
   session=`expr $session + 1`
-
+  set +f; unset IFS
 done
